8891e459f4d9f6f3bed53fc86b2d61a85375f8a6
yinm <yinm.cminor9@gmail.com>
2018-12-13 21:24:25 +0900

    Add API

---
+++ sampple-app/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const server = app.listen(3000, () => {
+  console.log('Node.js is listening to PORT:' + server.address().port)
+})
+
+const photoList = [
+  {
+    id: '001',
+    name: 'photo001.jpg',
+    type: 'jpg',
+    dataUrl: 'http://localhost:3000/data/photo001.jpg'
+  },
+  {
+    id: '002',
+    name: 'photo002.jpg',
+    type: 'jpg',
+    dataUrl: 'http://localhost:3000/data/photo002.jpg'
+  }
+]
+
+app.get('/api/photo/list', (req, res, next) => {
+  res.json(photoList)
+})
