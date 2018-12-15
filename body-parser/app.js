79fd458c5b613abe214314e9b150287e05d91ff4
yinm <yinm.cminor9@gmail.com>
2018-12-15 09:42:11 +0900

    Add body-parser

--- body-parser/app.js
+++ body-parser/app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 
 app.listen(3000)
 console.log('Server is online.')
 
 app.post('/', (req, res) => {
-  res.send('POST is sended.')
+  console.log(req.body)
+  console.log(req.body.name)
+
+  res.send('POST request to the homepage')
 })
