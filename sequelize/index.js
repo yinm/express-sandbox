9210ef4e5bc0f6d91563152e4dab938cf88df004
yinm <yinm.cminor9@gmail.com>
2018-12-16 13:21:50 +0900

    Check 2nd

--- sequelize/index.js
+++ sequelize/index.js
@@ -1,5 +1,6 @@
+// 2nd
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('NodeTest', 'root', 'password', {dialect: 'mysql'})
+const sequelize = new Sequelize('NodeTest', 'root', 'password', { dialect: 'mysql' })
 sequelize.query('select * from test_table').spread((results, metadata) => {
   console.log(results)
   sequelize.close()
