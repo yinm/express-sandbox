136949d12ce0c0d998799d6085b61cdc0a5617df
yinm <yinm.cminor9@gmail.com>
2018-12-17 09:09:53 +0900

    Add middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+function myLogger(req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
